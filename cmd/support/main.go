091d6a16acfabe842fbbaec356107dc3feffcfa0
usysrc <tilmann.hars@googlemail.com>
2023-05-27 16:55:50 +0200

    moved db setup in own package

--- cmd/support/main.go
+++ cmd/support/main.go
@@ -3,41 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
-
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 
 	"github.com/usysrc/support/internal/api"
-	"github.com/usysrc/support/internal/models"
+	"github.com/usysrc/support/internal/database"
 )
 
 func main() {
-	// Connect to the database
-	// Get database connection configuration from environment variables
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	sslMode := os.Getenv("DB_SSLMODE")
-
-	// Construct DSN string
-	dsn := "host=" + dbHost +
-		" user=" + dbUser +
-		" password=" + dbPassword +
-		" dbname=" + dbName +
-		" port=" + dbPort +
-		" sslmode=" + sslMode
-
-	// Connect to the database
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	// Run migrations
-	db.AutoMigrate(&models.Ticket{})
+	// Set up the Postgres database
+	db := database.SetupDatabase()
 
 	// Set up the Gin router
 	router := api.SetupRouter(db)
---
+++ internal/database/setup.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"os"
+
+	"github.com/usysrc/support/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func SetupDatabase() *gorm.DB {
+	// Connect to the database
+	// Get database connection configuration from environment variables
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	sslMode := os.Getenv("DB_SSLMODE")
+
+	// Construct DSN string
+	dsn := "host=" + dbHost +
+		" user=" + dbUser +
+		" password=" + dbPassword +
+		" dbname=" + dbName +
+		" port=" + dbPort +
+		" sslmode=" + sslMode
+
+	// Connect to the database
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Run migrations
+	migrationErr := db.AutoMigrate(&models.Ticket{})
+	if migrationErr != nil {
+		panic("failed to migrate database")
+	}
+
+	return db
+}
